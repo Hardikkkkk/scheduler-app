58f8550b75c018aa2a57137320f14a6585d95334
Hardikkkkk <bhondvehardik@gmail.com>
2025-09-22 20:26:36 +0530

    db fix

--- backend/knexfile.d.ts
+++ backend/knexfile.d.ts
@@ -1,4 +1,9 @@
-declare module '../knexfile' {
-  const config: any;
-  export default config;
-}
+import knex from 'knex';
+import knexConfig from '../knexfile';
+
+const environment = process.env.NODE_ENV || 'development';
+const config = knexConfig[environment];
+
+const db = knex(config);
+
+export default db;
