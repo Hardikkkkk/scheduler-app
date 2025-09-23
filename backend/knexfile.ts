048d462883cc884b354dfcef350129592358395d
Hardikkkkk <bhondvehardik@gmail.com>
2025-09-23 13:12:14 +0530

    fixed

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -34,25 +34,19 @@ const config = {
     migrations: {
       tableName: 'knex_migrations',
     },
+    
   },
 
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL || {
-      database: process.env.DB_NAME || 'scheduler',
-      user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD?.toString() || '1234',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 5432,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+production: {
+  client: 'postgresql',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   },
+  pool: { min: 2, max: 10 },
+  migrations: { tableName: 'knex_migrations' },
+},
+
 };
 
 export default config;
